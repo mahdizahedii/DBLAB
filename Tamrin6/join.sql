c32626c0caeed5a04abb1283ec25652283e2f5ee
Mahdi_Zahedi <59919435+mahdizahedii@users.noreply.github.com>
2023-06-12 13:30:12 +0330

    Delete Tamrin6 directory

--- Tamrin6/join.sql
+++
@@ -1,3 +0,0 @@
-SELECT COUNT(reservation.carID)AS 'COUNT',reservation.carID,car.brand,car.model,car.typelabel FROM `reservation` 
-    INNER JOIN car ON reservation.carID=car.carID
-    GROUP BY carID
